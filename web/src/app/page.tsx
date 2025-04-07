a41a82bec2102b88d5f4e87cfaedfdf9fe310d0b
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-04-07 21:45:59 +0800

    Improve listing design

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,7 @@
 import {getListings} from "@web/grpc/client";
 import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@web/components/carousel";
+import Link from "next/link";
+import {Button} from "@web/components/button";
 
 type Props = {
     searchParams?: {
@@ -14,24 +16,38 @@ export default async function Page(props: Props) {
     const {listings, pageInfo} = await getListings({after, before, limit})
 
     return (
-        <div className="grid grid-cols-3 gap-6 p-6">
-            {listings.map((listing) => (
-                <div key={listing.id} className="flex flex-col gap-2">
-                    <Carousel className="rounded-lg overflow-hidden">
-                        <CarouselContent>
-                            {listing.imageUrls.map((url, index) => (
-                                <CarouselItem key={index}>
-                                    <img src={url} alt="property preview"/>
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious/>
-                        <CarouselNext/>
-                    </Carousel>
-                    <h4 className="font-semibold truncate">{listing.address}</h4>
-                    <p className="font-semibold">₱ {formatToPhilippineNumeric(listing.price)}</p>
-                </div>
-            ))}
+        <div className="py-16">
+            <div className="grid grid-cols-3 gap-6 p-6">
+                {listings.map((listing) => (
+                    <div key={listing.id} className="flex flex-col gap-2">
+                        <Carousel className="rounded-lg overflow-hidden">
+                            <CarouselContent>
+                                {listing.imageUrls.map((url, index) => (
+                                    <CarouselItem key={index}>
+                                        <img src={url} alt="property preview"/>
+                                    </CarouselItem>
+                                ))}
+                            </CarouselContent>
+                            <CarouselPrevious/>
+                            <CarouselNext/>
+                        </Carousel>
+                        <h4 className="font-semibold truncate">{listing.address}</h4>
+                        <p className="font-semibold">₱ {formatToPhilippineNumeric(listing.price)}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="flex">
+                <Button asChild className="w-full" disabled={pageInfo?.hasPrevPage}>
+                    <Link href={`/?before=${pageInfo?.startCursor}&limit=${limit}`}>
+                        Previous
+                    </Link>
+                </Button>
+                <Button asChild className="w-full" disabled={pageInfo?.hasNextPage}>
+                    <Link href={`/?after=${pageInfo?.endCursor}&limit=${limit}`}>
+                        Next
+                    </Link>
+                </Button>
+            </div>
         </div>
     )
 }
