0708451cea00c1282f3ee800ea4d08a7600417cd
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-04-13 22:51:30 +0800

    Debug

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"homagochi/internal/cron"
 	"homagochi/internal/grpc"
 	"homagochi/internal/source/pagibig"
 	"log"
+	"os"
 )
 
 func main() {
 	_ = godotenv.Load()
+
+	fmt.Println(os.Getenv("ENV"))
+	fmt.Println(os.Getenv("NEON_DATABASE_URL"))
+	fmt.Println(os.Getenv("GCP_PROJECT_ID"))
+	fmt.Println(os.Getenv("GCP_BUCKET_NAME"))
 
 	if err := (&pagibig.ScrapeListingJob{}).Run(); err != nil {
 		log.Fatal(err)
