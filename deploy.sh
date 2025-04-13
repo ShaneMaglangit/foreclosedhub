33fc2c29c137c949e333c0957c1a2a312e57937f
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-04-13 19:01:34 +0800

    Fix pipelines?

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,9 @@
 # Variables
 INSTANCE_NAME="server"
 USER="ubuntu"
-LOCAL_BINARY_PATH="../backend/app"
+LOCAL_BINARY_PATH="./app"
 REMOTE_BINARY_PATH="/home/$USER/app"
 SERVICE_NAME="app"
-
-echo $GCP_ZONE
-echo $USER
-echo $INSTANCE_NAME
-echo $REMOTE_BINARY_PATH
-echo $NEON_DATABASE_URL
-echo $GCP_PROJECT_ID
-echo $GCP_BUCKET_NAME
 
 echo "Authenticating SSH"
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
