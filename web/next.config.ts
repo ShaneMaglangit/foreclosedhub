369eb35ef04a1aadac9697d4b56c21c70b899371
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-04-15 00:05:14 +0800

    Try to trace the proto file

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,17 +1,19 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-                port: '',
-                pathname: '/*-homagochi/**',
-                search: '',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/*-homagochi/**",
+        search: "",
+      },
+    ],
+  },
+  outputFileTracingRoot: path.join(process.cwd(), "/proto"),
 };
 
 export default nextConfig;
