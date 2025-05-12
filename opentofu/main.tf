eb69c15e5f33c5e071ac01763af01ed2b09ef8fc
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-05-13 00:20:02 +0800

    Update EC2 network rules

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -78,11 +78,62 @@ resource "aws_key_pair" "server" {
   key_name   = "ci-deploy-key"
   public_key = tls_private_key.ssh.public_key_openssh
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "server" {
-  ami           = "ami-0e8ebb0ab254bb563"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.server.key_name
+  ami                         = "ami-0e8ebb0ab254bb563"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.server.key_name
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.ssh.id]
+  associate_public_ip_address = true
 }
 
 provider "vercel" {
