9dc1ce06df5f6c9d3f25ace76c359e58b73639d4
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-05-12 22:42:08 +0800

    Fix opentofu format errors

--- opentofu/variables.tf
+++ opentofu/variables.tf
@@ -1,29 +1,29 @@
-variable gitlab_token {
+variable "gitlab_token" {
   description = "Gitlab Access Token"
   type        = string
 }
 
-variable vercel_token {
+variable "vercel_token" {
   description = "Vercel Access Token"
   type        = string
 }
 
-variable vercel_team_id {
+variable "vercel_team_id" {
   description = "Vercel Team ID"
   type        = string
 }
 
-variable aws_access_key {
+variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
 }
 
-variable aws_secret_access_key {
+variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
   type        = string
 }
 
-variable aws_region {
+variable "aws_region" {
   description = "AWS Region"
   default     = "ap-southeast-1"
   type        = string
