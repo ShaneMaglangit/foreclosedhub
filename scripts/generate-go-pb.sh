f2b4b669ee58acb62439c7a28d8e6b1b2f6ddf9a
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-05-14 22:28:11 +0800

    Initialize GRPC server

---
+++ scripts/generate-go-pb.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+protoc --go_out=./server/internal/ --go_opt=paths=import --go-grpc_out=./server/internal/ --go-grpc_opt=paths=import ./proto/hello.proto
--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"server/internal/proto"
+)
+import "google.golang.org/grpc"
+
+const defaultPort = "50051"
+
+type HelloServiceServer struct {
+	proto.UnimplementedHelloServiceServer
+}
+
+func (s *HelloServiceServer) SayHello(ctx context.Context, req *proto.SayHelloRequest) (*proto.SayHelloResponse, error) {
+	return &proto.SayHelloResponse{Body: "Hello world"}, nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+	port := os.Getenv("GRPC_PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	var server *grpc.Server
+
+	server = grpc.NewServer()
+	log.Println("Starting gRPC server in development mode (no TLS)")
+
+	proto.RegisterHelloServiceServer(server, &HelloServiceServer{})
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	if err = server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
