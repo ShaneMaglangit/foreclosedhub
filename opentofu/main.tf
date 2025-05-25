745d990054a5f3cd397503296677849623b3ec86
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-05-25 17:31:07 +0800

    Revert "Down AWS resources"
    This reverts commit 5e4793731a1bafc0e36ea139f0cbe7fd03d0dc6e.

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -76,90 +76,90 @@ resource "gitlab_project_variable" "aws_region" {
   protected = false
 }
 
-# provider "aws" {
-#   region     = var.aws_region
-#   access_key = var.aws_access_key
-#   secret_key = var.aws_secret_access_key
-# }
-#
-# resource "aws_key_pair" "ci_ssh" {
-#   key_name   = "ci-deploy-key"
-#   public_key = tls_private_key.ci_ssh.public_key_openssh
-# }
-#
-# resource "aws_vpc" "app" {
-#   cidr_block = "10.0.0.0/16"
-# }
-#
-# resource "aws_internet_gateway" "app" {
-#   vpc_id = aws_vpc.app.id
-# }
-#
-# resource "aws_subnet" "public_1a" {
-#   vpc_id                  = aws_vpc.app.id
-#   cidr_block              = "10.0.1.0/24"
-#   map_public_ip_on_launch = true
-# }
-#
-# resource "aws_route_table" "public" {
-#   vpc_id = aws_vpc.app.id
-#
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.app.id
-#   }
-# }
-#
-# resource "aws_route_table_association" "public_1a" {
-#   subnet_id      = aws_subnet.public_1a.id
-#   route_table_id = aws_route_table.public.id
-# }
-#
-# resource "aws_security_group" "app_allow_inbound" {
-#   name        = "app-allow-inbound"
-#   description = "Allow inbound traffic for gRPC and SSH"
-#   vpc_id      = aws_vpc.app.id
-#
-#   ingress {
-#     description = "gRPC"
-#     from_port   = 50051
-#     to_port     = 50051
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   ingress {
-#     description = "SSH"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-#
-# resource "aws_instance" "server" {
-#   ami                         = "ami-01938df366ac2d954"
-#   instance_type               = "t2.micro"
-#   key_name                    = aws_key_pair.ci_ssh.key_name
-#   subnet_id                   = aws_subnet.public_1a.id
-#   vpc_security_group_ids      = [aws_security_group.app_allow_inbound.id]
-#   associate_public_ip_address = true
-# }
-#
-# output "server_id" {
-#   value = aws_instance.server.id
-# }
-#
-# output "server_public_ip" {
-#   value = aws_instance.server.public_ip
-# }
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_key_pair" "ci_ssh" {
+  key_name   = "ci-deploy-key"
+  public_key = tls_private_key.ci_ssh.public_key_openssh
+}
+
+resource "aws_vpc" "app" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "app" {
+  vpc_id = aws_vpc.app.id
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.app.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.app.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app.id
+  }
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "app_allow_inbound" {
+  name        = "app-allow-inbound"
+  description = "Allow inbound traffic for gRPC and SSH"
+  vpc_id      = aws_vpc.app.id
+
+  ingress {
+    description = "gRPC"
+    from_port   = 50051
+    to_port     = 50051
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "server" {
+  ami                         = "ami-01938df366ac2d954"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.ci_ssh.key_name
+  subnet_id                   = aws_subnet.public_1a.id
+  vpc_security_group_ids      = [aws_security_group.app_allow_inbound.id]
+  associate_public_ip_address = true
+}
+
+output "server_id" {
+  value = aws_instance.server.id
+}
+
+output "server_public_ip" {
+  value = aws_instance.server.public_ip
+}
 
 provider "vercel" {
   api_token = var.vercel_token
