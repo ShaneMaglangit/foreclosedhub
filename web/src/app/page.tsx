428c2bd090d17c0c353aca47cd574b0b9f668702
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-06-06 23:14:24 +0800

    Remove GRPC client

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,4 @@
-import { z } from "zod"
-import { getListingsInBoundary } from "@web/lib/grpc/getListingsInBoundary";
 import Map from "@web/app/map";
-import { execute } from "@web/lib/graphql/execute";
-import { GetListingsQuery } from "@web/lib/graphql/getListings";
-import { useQuery } from '@tanstack/react-query'
 import QueryProvider from "@web/app/queryProvider"
 
 
--- web/src/lib/grpc/client.ts
+++
@@ -1,6 +0,0 @@
-import * as grpc from '@grpc/grpc-js';
-import {listing} from "@web/lib/proto/listing";
-import ListingServiceClient = listing.ListingServiceClient;
-import { env } from '@web/env';
-
-export const client = new ListingServiceClient(env.GRPC_ADDRESS, grpc.credentials.createInsecure());
