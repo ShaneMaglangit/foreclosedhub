6fceb0367ae985bda042ee8f80ce6f664f56c807
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-06-06 23:17:57 +0800

    Remove protobuf

--- scripts/generate-pb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-protoc --go_out=./server/internal/ --go_opt=paths=import --go-grpc_out=./server/internal/ --go-grpc_opt=paths=import ./proto/*.proto
-
-cd web || exit
-yarn run proto:generate
