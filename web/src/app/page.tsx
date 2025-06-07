de1ff92128ec1993feb6608ccfd6b776f490540f
ShaneMaglangit <shanedmaglangit@gmail.com>
2025-06-07 09:58:47 +0800

    Wrap map in suspense boundary

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Map from "@web/app/map";
 import QueryProvider from "@web/app/queryProvider"
-
+import { Suspense } from 'react'
 
 export default async function Page() {
     return (
         <QueryProvider>
-            <Map className="w-dvw h-dvh" />
+            <Suspense fallback={<div className="w-dvw h-dvh flex items-center justify-center">Loading...</div>}>
+                <Map className="w-dvw h-dvh" />
+            </Suspense>
         </QueryProvider>
     )
 }
